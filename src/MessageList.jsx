964d65e0806eb17eeb148e8fce066c60581fb951
Couragyn Chretien <couragyn@gmail.com>
2017-04-04 01:08:29 +0000

    copied code from site, made moduler

---
+++ src/MessageList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </div>
+    );
+  }
+}
+export default MessageList;
